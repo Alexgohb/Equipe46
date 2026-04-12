418003705a4764b35fbccbd5260c15b458fbfc7e
Marc-Olivier Beaudry <mobea82@ulaval.ca>
2026-04-12 15:11:15 -0400

    new file:   __pycache__/rk4.cpython-313.pyc 	new file:   __pycache__/splines_edo_implicite.cpython-313.pyc 	modified:   "\303\251quipe46.py"

--- équipe46.py
+++ équipe46.py
@@ -3,3 +3,11 @@ import matplotlib.pyplot as plt
 from splines_edo_implicite import splines_edo_implicite
 from rk4 import rk4
 
+
+#Question 1 c)
+alpha_c = (np.sqrt(2) * np.exp(np.pi/4))/2
+beta_c = (np.sqrt(2) * np.exp(np.pi/4))
+f_c = lambda x, y, dy: -2*np.exp(-x) / np.sin(x) * y * dy - 2*np.exp(x)*np.sin(x)
+
+print(splines_edo_implicite(alpha=alpha_c, beta=beta_c, f=f_c, t0=(np.pi/4), tf=3, N=16))
+
