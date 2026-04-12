5b1f7046b52cb6708e1e640e10206d631b1d8e19
Marc-Olivier Beaudry <mobea82@ulaval.ca>
2026-04-12 16:35:54 -0400

    modified:   "\303\251quipe46.py"

--- équipe46.py
+++ équipe46.py
@@ -9,5 +9,17 @@ alpha_c = (np.sqrt(2) * np.exp(np.pi/4))/2
 beta_c = (np.sqrt(2) * np.exp(np.pi/4))
 f_c = lambda x, y, dy: -2*np.exp(-x) / np.sin(x) * y * dy - 2*np.exp(x)*np.sin(x)
 
-print(splines_edo_implicite(alpha=alpha_c, beta=beta_c, f=f_c, t0=(np.pi/4), tf=3, N=16))
+coeffs = splines_edo_implicite(alpha=alpha_c, beta=beta_c, f=f_c, t0=(np.pi/4), tf=3, N=16)
+t = np.linspace(np.pi/4, 3, 17)
+y = lambda t: np.exp(t)*np.sin(t)
+plt.figure()
+for i in range(len(coeffs)):
+    plt.plot(np.linspace(np.pi/4, 3, 17), coeffs[i][0]*t[i]**3 + coeffs[i][1]*t[i]**2 + coeffs[i][2]*t[i] + coeffs[i][3]
+    , '-o', label=f'Spline {i}', color = 'orange')
 
+#y = lambda t:np.exp(t)*np.sin(t)
+#plt.plot(np.linspace(np.pi/4, 3, 17), y(np.linspace(np.pi/4, 3, 17)), label='Solution exacte', color='black')
+#plt.title("Splines pour l'EDO implicite")
+#plt.xlabel('t')
+#plt.ylabel('y(t)')
+plt.show()
